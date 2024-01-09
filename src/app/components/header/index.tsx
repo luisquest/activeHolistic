b3ebd198ff0a3f2b236086885b0f1ee520cadcba
Pablo Cesar Vega Valverde <pcvv@tinnto.com>
2024-01-08 22:59:18 -0500

    [ADD] Agregar componentes header y carrousel

---
+++ src/app/components/header/index.tsx
@@ -0,0 +1,96 @@
+'use client'
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+
+
+const Header = () => {
+    const [openMenu, setOpenMenu] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth > 768) { // Tamaño MD en píxeles
+                setOpenMenu(false);
+            }
+        };
+
+        // Agrega el listener para el evento resize
+        window.addEventListener('resize', handleResize);
+
+        // Limpia el listener cuando el componente se desmonta
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []); // Se ejecuta solo al montar el componente
+
+    return (
+        <header>
+            <div className="relative">
+                {/* TOP TABLET */}
+                <div className="hidden md:flex flex-col lg:flex-row items-center lg:justify-around ">
+                    <div className="flex flex-row gap-8 ">
+                        <div>support@cybron-systems.com</div>
+                        <div>13 Division st, New York, 16004</div>
+                        <div>+1 800 777 000</div>
+                    </div>
+                    <div className="hidden lg:flex flex-row gap-8 ">
+                        <div>AA</div>
+                        <div>BB <span>+99</span></div>
+                        <div>CC</div>
+                    </div>
+                </div>
+
+                <div className={`relative ${openMenu ? 'hidden' : 'flex'} flex-row gap-4 justify-center py-4 min-h-24 `}>
+                    <div className="self-center">
+                        <Image src="/assets/images/logo/logo.png"
+                            alt="Cybron WordPress Theme"
+                            sizes="100vw"
+                            width={170}
+                            height={38}
+                        />
+                    </div>
+
+                    <button className="absolute lg:hidden right-6 top-1/4 translate-y-1/2"
+                        onClick={() => setOpenMenu(!openMenu)}
+                    >
+                        <Image src="/assets/icons/menu.svg"
+                            alt="Menu"
+                            width={27}
+                            height={34}
+                            priority
+                        />
+                    </button>
+                </div>
+
+
+                {/* MENU OPEN */}
+                {openMenu &&
+                    <div className="absolute bg-blue-950 min-h-screen min-w-full p-4">
+                        <div className={`relative flex-row gap-4 justify-center py-4 min-h-24`}>
+                            <div className="flex items-center justify-center">
+                                <Image src="/assets/images/logo/logo-light.png"
+                                    alt="Cybron WordPress Theme"
+                                    sizes="100vw"
+                                    width={170}
+                                    height={38}
+                                />
+                            </div>
+
+                            <button className="absolute lg:hidden right-3 top-4"
+                                onClick={() => setOpenMenu(!openMenu)}
+                            >
+                                <Image src="/assets/icons/menu.svg"
+                                    alt="Menu"
+                                    width={27}
+                                    height={34}
+                                    priority
+                                />
+                            </button>
+                        </div>
+                    </div>}
+            </div>
+        </header>
+    );
+}
+
+export default Header;
