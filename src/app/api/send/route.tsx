785ee7c3bbefeb386be6a1698d63f6b316bce077
Pablo Cesar Vega Valverde <pvegav@autonoma.edu.pe>
2024-01-22 00:29:39 -0500

    [UPDATE] Utilizar token y correo RESEND desde ENV

--- src/app/api/send/route.tsx
+++ src/app/api/send/route.tsx
@@ -2,7 +2,8 @@ import { Resend } from 'resend';
 import { NextResponse } from 'next/server';
 import EmailTemplate, { EmailTemplateProps } from '@/app/components/email/template';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY ?? 're_cg5qikbQ_5iPpi3jREgTnvUwCuo5ngmcL');
+const resendEmail = process.env.RESEND_EMAIL ?? 'gatito_190lobo_teve@hotmail.com';
 
 export async function POST(request: Request) {
     const requestData: EmailTemplateProps = await request.json();
@@ -11,7 +12,7 @@ export async function POST(request: Request) {
     try {
         const data = await resend.emails.send({
             from: 'Not reply <onboarding@resend.dev>',
-            to: ['gatito_190lobo_teve@hotmail.com'],
+            to: [resendEmail],
             subject: requestData.subject,
             react: EmailTemplate(requestData),
             text: 'it works!',
