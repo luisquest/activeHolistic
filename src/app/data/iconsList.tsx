444d10d596b08999719fc98fef8af9be34cd5573
Pablo Cesar Vega Valverde <pvegav@autonoma.edu.pe>
2024-01-22 22:44:04 -0500

    [UPDATE] Actualizar SectionIconList

--- src/app/data/iconsList.tsx
+++ src/app/data/iconsList.tsx
@@ -1,22 +1,26 @@
+import ClockIcon from "../components/icons/clock";
+import MailIcon from "../components/icons/mail";
+import MapPinIcon from "../components/icons/map-pin";
+import PhoneIcon from "../components/icons/phone";
 import IIconsList from "./interfaces/iiconslist";
 
 const IconsListData: IIconsList[] = [
     {
-        iconImage: "https://flowbite.com/docs/images/carousel/carousel-1.svg",
+        iconImage: <MapPinIcon className="size-16 text-blue-600" />,
         title: "Location",
         subtitle: [
             "29 Nicolas str, New York,", "987597-50"
         ]
     },
     {
-        iconImage: "https://flowbite.com/docs/images/carousel/carousel-2.svg",
+        iconImage: <PhoneIcon className="size-16 text-blue-600" />,
         title: "Phones",
         subtitle: [
             "0(800) 890-90-609", "0(800) 890-90-620"
         ]
     },
     {
-        iconImage: "https://flowbite.com/docs/images/carousel/carousel-3.svg",
+        iconImage: <MailIcon className="size-16 text-blue-600" />,
         title: "Email",
         subtitle: [
             "sale@like-themes.com", "tech@like-themes.com"
@@ -24,7 +28,7 @@ const IconsListData: IIconsList[] = [
 
     },
     {
-        iconImage: "https://flowbite.com/docs/images/carousel/carousel-4.svg",
+        iconImage: <ClockIcon className="size-16 text-blue-600" />,
         title: "Working Hours",
         subtitle: [
             "Wednesday - Sunday", "7:00 AM - 5:00 PM"
