afd190c48d322c846b929a7fe4b428af923cc516
Pablo Cesar Vega Valverde <pvegav@autonoma.edu.pe>
2024-01-27 12:06:33 -0500

    [UPDATE] Transformar codigo en sections

---
+++ src/app/sections/beneficts.tsx
@@ -0,0 +1,23 @@
+import Beneficts from "../components/list/beneficts/beneficts";
+
+const SectionBeneficts = () => {
+    return (
+        <section className="flex bg-blue-200 w-full flex-col justify-center items-center py-20">
+            <div className="container">
+                <div className="flex flex-col gap-12 items-center py-20 px-20">
+                    <div className="text-center">
+                        <h6 className="font-semibold">CLINIC BENEFITS</h6>
+                        <h2 className="text-6xl font-bold mt-3">
+                            Our Capabilities
+                        </h2>
+                    </div>
+                </div>
+                <div>
+                    <Beneficts />
+                </div>
+            </div>
+        </section >
+    );
+}
+
+export default SectionBeneficts;
