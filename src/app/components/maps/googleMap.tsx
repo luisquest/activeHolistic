38970e60bd799be2b1124f07f4e36e01c913b22e
Pablo Cesar Vega Valverde <pvegav@autonoma.edu.pe>
2024-02-05 22:45:19 -0500

    [UPDATE] Hacer googleMaps dinámico

---
+++ src/app/components/maps/googleMap.tsx
@@ -0,0 +1,9 @@
+const GoogleMap = () => {
+    return (
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1159.590520315067!2d-76.9826389534399!3d-12.154372421834694!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105b84113003ee7%3A0x1fd71e1502ba8da6!2sMall%20del%20Sur!5e0!3m2!1ses!2spe!4v1705376493423!5m2!1ses!2spe"
+            height="600" className="border-0 w-full" allowFullScreen loading="eager" referrerPolicy="no-referrer-when-downgrade"></iframe>
+    );
+}
+
+export default GoogleMap;
