a9f01a9c2ef1840e0140b6ffe48d48aa06005bc1
Pablo Cesar Vega Valverde <pvegav@autonoma.edu.pe>
2024-02-26 01:29:09 -0500

    [CONFIG] Configurar authOptions y Providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Providers from './providers/providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,13 +11,17 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout({
-  children,
+  children
 }: {
   children: React.ReactNode
 }) {
   return (
     <html lang="es">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
     </html>
   )
 }
