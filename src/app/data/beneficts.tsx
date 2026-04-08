2dfe069c3d5f89f8ac811a429e5433f388f9739f
Luis Ramos <lramos.quest@gmail.com>
2026-04-08 17:08:41 -0500

    cambios texto en servicios

--- src/app/data/beneficts.tsx
+++ src/app/data/beneficts.tsx
@@ -4,26 +4,26 @@ const BenefictsData: IBeneficts[] = [
     {
         iconImage: "https://flowbite.com/docs/images/carousel/carousel-1.svg",
         title: "FISIOTERAPIA ESPECIALIZADA",
-        description: "With prosthetics, we have the power to help people defy their limitations.",
+        description: "Recupera tu movimiento, mejora tu calidad de vida",
         backImage: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
     },
     {
         iconImage: "https://flowbite.com/docs/images/carousel/carousel-2.svg",
-        title: "TERAPIA MANUAL ORTOPÉDICA",
-        description: "Future is a testament to human ingenuity and resilience.",
+        title: "OSTEOPATIA",
+        description: "Vive mejor, desde la causa.",
         backImage: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
     },
     {
         iconImage: "https://flowbite.com/docs/images/carousel/carousel-3.svg",
         title: "ELECTRO NEURO ACUPUNTURA",
-        description: "Creating the bridge that connects physical disability to ability",
+        description: "Regula tu sistema nervioso",
         backImage: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 
     },
     {
         iconImage: "https://flowbite.com/docs/images/carousel/carousel-3.svg",
-        title: "ESCOLIOSIS MÉTODO RIGO- BSPTS",
-        description: "Prosthetics is a reminder that limitations are only temporary,.",
+        title: "CURSOS",
+        description: "Sé el mejor.",
         backImage: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
     }
 ];
