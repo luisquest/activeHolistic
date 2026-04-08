830d74c283cf0194621e46e59edb1f927af32fd1
Luis Ramos <lramos.quest@gmail.com>
2026-04-08 17:21:37 -0500

    cambios texto en servicios

--- src/app/sections/ourServices.tsx
+++ src/app/sections/ourServices.tsx
@@ -18,10 +18,10 @@ const SectionOurServices = () => {
                             No somos una clínica más. Sabemos lo frustrante que es vivir con dolor y no encontrar una solución real.<br />
 Por eso trabajamos diferente: buscamos la causa de tu problema para ayudarte a resolverlo de verdad.
                         </p>
-                        <button className="rounded-full px-14 min-w-48 py-5 m-1 overflow-hidden relative group cursor-pointer font-medium bg-blue-500 text-blue-950">
+                       {/* <button className="rounded-full px-14 min-w-48 py-5 m-1 overflow-hidden relative group cursor-pointer font-medium bg-blue-500 text-blue-950"> 
                             <span className="absolute w-60 h-0 transition-all duration-300 origin-center rotate-90 -translate-x-20 bg-black top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"></span>
                             <span className="relative transition duration-300 group-hover:text-white ease text-nowrap">Productos →</span>
-                        </button>
+                        </button> */}
                     </div>
                     {/* PRODUCTS */}
                     <div className="flex flex-col lg:flex-row items-center">
